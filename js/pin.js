d39845b1bdd07619d7fd79584dd38c6df10c5536
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2020-02-18 23:45:57 +0500

    Реализовал получение данных с сервера, отключил использование моков

--- js/pin.js
+++ js/pin.js
@@ -9,13 +9,15 @@
   /**
    * Создаёт DOM-элемент метки на карте для последующего добавления на карту
    * @param {*} mapPin - шаблон разметки, который будет заполняться данными
+   * @param {integer} pinNumber - номер метки
    * @return {*} шаблон метки с заполненными данными
    */
-  var renderPin = function (mapPin) {
+  var renderPin = function (mapPin, pinNumber) {
     var mapPinElement = mapPinTemplate.cloneNode(true);
     mapPinElement.querySelector('img').src = mapPin.author.avatar;
     mapPinElement.querySelector('img').alt = mapPin.offer.title;
     mapPinElement.style = 'left: ' + (mapPin.location.x - window.map.pin.OFFSET_X) + 'px; top: ' + (mapPin.location.y - window.map.pin.OFFSET_Y) + 'px;';
+    mapPinElement.dataset.number = pinNumber;
 
     return mapPinElement;
   };
